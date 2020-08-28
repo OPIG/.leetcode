07843b5f237526d6bad9bbbd49077f195e6f88ca
pinggui.yuan <pinggui.yuan>
2020-08-28 10:26:49 +0800

    leetcode practice

--- dist/9.palindrome-number.dev.js
+++ dist/9.palindrome-number.dev.js
@@ -1,10 +1,12 @@
+"use strict";
+
 /*
  * @lc app=leetcode id=9 lang=javascript
  *
  * [9] Palindrome Number
  */
-
 // @lc code=start
+
 /**
  * @param {number} x
  * @return {boolean}
@@ -18,34 +20,29 @@
 //     }
 //     return true
 // };
-
-
 //resolution 2
 // var isPalindrome = function(x) {
 //     if(x<0) return false
-
 //     let res = 0
 //     let originX = x
-
 //     while(x){
 //         res = res*10 + x%10
 //         x = parseInt(x/10)
 //     }
-    
 //     return originX === res? true: false
 // };
-
-
 //resolution 3
-var isPalindrome = function (x){
-    if(x < 0) return false
-    let ren =0
-    for(let i = x; i>=1; i = Math.floor(i/10)){
-        ren = ren*10 + i%10
-    }
+var isPalindrome = function isPalindrome(x) {
+  if (x < 0) return false;
+  var ren = 0;
 
-    return ren === x
-}
-// @lc code=end
-let test = isPalindrome(-121)
-console.log(test)
+  for (var i = x; i >= 1; i = Math.floor(i / 10)) {
+    ren = ren * 10 + i % 10;
+  }
+
+  return ren === x;
+}; // @lc code=end
+
+
+var test = isPalindrome(-121);
+console.log(test);
